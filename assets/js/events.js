e8d313e50a724108609b1fd7c9c69f329e7e4d4f
Sergej Schatz <sergej.schatz@cgm.com>
2025-03-17 21:27:51 +0100

    further commit

--- assets/js/events.js
+++ assets/js/events.js
@@ -5,13 +5,6 @@ document.addEventListener("DOMContentLoaded", async function () {
     const imageElement = document.createElement("img");
     imageElement.classList.add("event-image");
 
-    const leftButton = document.createElement("button");
-    leftButton.classList.add("event-nav", "left");
-    leftButton.innerHTML = "&lt;";
-
-    const rightButton = document.createElement("button");
-    rightButton.classList.add("event-nav", "right");
-    rightButton.innerHTML = "&gt;";
 
     const dotsContainer = document.createElement("div");
     dotsContainer.classList.add("event-dots");
@@ -50,25 +43,13 @@ document.addEventListener("DOMContentLoaded", async function () {
             dotsContainer.appendChild(dot);
         });
 
-        leftButton.addEventListener("click", function () {
-            currentIndex = (currentIndex - 1 + events.length) % events.length;
-            updateImage();
-        });
-
-        rightButton.addEventListener("click", function () {
-            currentIndex = (currentIndex + 1) % events.length;
-            updateImage();
-        });
-
         updateImage(); // Erstes Bild setzen
 
     } catch (error) {
         console.error("Fehler beim Laden der Events:", error);
     }
 
-    eventContainer.appendChild(leftButton);
     eventContainer.appendChild(imageElement);
-    eventContainer.appendChild(rightButton);
 
     document.querySelector(".tile-aktuell:first-child").appendChild(eventContainer);
     document.querySelector(".tile-aktuell:first-child").appendChild(dotsContainer);
