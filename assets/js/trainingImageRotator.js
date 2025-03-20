4c24d7edbfca85edc58a34a62b1ccc5f24438aeb
Sergej Schatz <sergej.schatz@cgm.com>
2025-03-20 21:35:28 +0100

    further commit

--- assets/js/trainingImageRotator.js
+++ assets/js/trainingImageRotator.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMContentLoaded", function () {
         console.error("Fehler: .training-image-container nicht gefunden!");
         return;
     }
-
     let images = [
         "/assets/images/training/training1.webp",
         "/assets/images/training/training2.webp",
