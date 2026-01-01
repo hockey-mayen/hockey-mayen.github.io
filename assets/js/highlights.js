9f1a4252ed3d1ad331f656bd6f1050cb8263a152
Sergej Schatz <sergej.schatz@cgm.com>
2026-01-01 10:36:33 +0100

    added images

--- assets/js/highlights.js
+++ assets/js/highlights.js
@@ -41,7 +41,7 @@ document.addEventListener("DOMContentLoaded", async function () {
         );
 
         function loadEntries() {
-            const maxLength = 500;
+            const maxLength = 550;
             const nextEntries = allEntries.slice(displayedEntries, displayedEntries + entriesPerLoad);
             const isInitialBatch = displayedEntries === 0;
 
